9c2cb8b71a39716c6a27698d6f82e20691b8bfe5
Kavya218 <84365948+Kavya218@users.noreply.github.com>
2021-06-03 23:26:10 +0530

    Add files via upload

---
+++ Rest_Assured/Activities/userGETResponse.java
@@ -0,0 +1,10 @@
+{
+    "id": 1654,
+    "username": "Kavya",
+    "firstName": "Paul",
+    "lastName": "Walker",
+    "email": "paulwalker@mail.com",
+    "password": "password133",
+    "phone": "9789509878",
+    "userStatus": 0
+}
