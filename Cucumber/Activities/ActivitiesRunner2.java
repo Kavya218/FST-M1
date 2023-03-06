5f23f9d054712f5d155cec1729fd6354e8de67f3
Kavya218 <84365948+Kavya218@users.noreply.github.com>
2023-03-06 13:22:56 +0530

    Update ActivitiesRunner2.java

--- Cucumber/Activities/ActivitiesRunner2.java
+++ Cucumber/Activities/ActivitiesRunner2.java
@@ -2,6 +2,7 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
+
 @RunWith(Cucumber.class)
 @CucumberOptions(
     features = "Features",
